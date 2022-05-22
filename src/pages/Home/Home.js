b52228da323fcb08079781c7025438fe462f3e07
Vitor Trimer <vitortrimer@gmail.com>
2022-05-22 01:21:19 -0300

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import Form from '../../components/Form/Form';
 import * as S from './Styles'
 
 const Home = () => {
@@ -7,9 +6,6 @@ const Home = () => {
   return(
     <S.HomeContainer>
       <S.MainContent>
-        <Form 
-          username=""
-        />
       </S.MainContent>
       <S.ProductsMenu>
         teste
